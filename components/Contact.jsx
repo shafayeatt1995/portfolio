148744bb598e8e289675162ab04e0595663dd3a9
shafayeatt1995 <shafayetalanik@gmail.com>
2024-03-11 10:22:57 +0600

    Updated

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,20 +12,20 @@ export default function Contact() {
       <div className="section-wrapper pr-60 pl-60 mb-60">
         <div className="contact-area bg-light-white-2">
           <h5 className="contact-title">
-            I'm always open to discussing produuct
+            I'm always open to discussing product
           </h5>
           <h5 className="">Design, Work or Partnerships.</h5>
           <form className="contact-form">
             <div className="form-input-item mb-12">
-              <label className="input-lebel name">name *</label>
+              <label className="input-label name">name *</label>
               <input name="name" className="input-box name h-6" type="text" />
             </div>
             <div className="form-input-item mb-12">
-              <label className="input-lebel gmail">Email *</label>
+              <label className="input-label gmail">Email *</label>
               <input name="email" className="input-box gmail" type="Email" />
             </div>
             <div className="form-input-item mb-12">
-              <label className="input-lebel message">Message *</label>
+              <label className="input-label message">Message *</label>
               <textarea
                 name="message"
                 className="input-box message"
